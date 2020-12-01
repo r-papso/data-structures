252b5e14fcfd9129574cda29a80e67499ddd63cb
r-papso <rastislav.papso@gmail.com>
2020-12-01 22:46:10 +0100

    Added missing documentation comments.

--- Structures/StructureFactory.cs
+++ Structures/StructureFactory.cs
@@ -58,26 +58,55 @@ namespace Structures
             return new KdTree<T>(data);
         }
 
+        /// <summary>
+        /// Loads instance of extendible hashing structure from <paramref name="directory"/>
+        /// </summary>
+        /// <typeparam name="T">Type of elements stored at extendible hashing</typeparam>
+        /// <param name="directory">Path to directory where the structure is stored</param>
+        /// <returns>Extendible hashing loaded from <paramref name="directory"/></returns>
         public IFileStructure<T> GetExtendibleHashing<T>(string directory) where T : ISerializable, new()
         {
             return new ExtendibleHashing<T>(directory);
         }
 
+        /// <summary>
+        /// Gets instance of extendible hashing structure
+        /// </summary>
+        /// <typeparam name="T">Type of elements stored at extendible hashing</typeparam>
+        /// <param name="directory">Path to directory where the structure will be stored</param>
+        /// <param name="clusterSize">File system's cluster size in bytes</param>
+        /// <returns>New extendible hashing that will be stored at <paramref name="directory"/></returns>
         public IFileStructure<T> GetExtendibleHashing<T>(string directory, int clusterSize) where T : ISerializable, new()
         {
             return new ExtendibleHashing<T>(directory, clusterSize);
         }
 
+        /// <summary>
+        /// Gets instance of hash set structure
+        /// </summary>
+        /// <typeparam name="T">Type of elements stored at hash set</typeparam>
+        /// <returns>Hash set structure</returns>
         public IStructure<T> GetHashSet<T>()
         {
             return new Hashing.HashSet<T>();
         }
 
+        /// <summary>
+        /// Gets hash set structure with specific initial capacity
+        /// </summary>
+        /// <typeparam name="T">Type of elements stored at hash set</typeparam>
+        /// <param name="initialCapacity"></param>
+        /// <returns>Hash set structure</returns>
         public IStructure<T> GetHashSet<T>(int initialCapacity)
         {
             return new Hashing.HashSet<T>(initialCapacity);
         }
 
+        /// <summary>
+        /// Gets instance of AVL tree structure
+        /// </summary>
+        /// <typeparam name="T">Type of elements stored at AVL tree</typeparam>
+        /// <returns>AVL tree structure</returns>
         public ITree<T> GetAvlTree<T>() where T : IComparable
         {
             return new AvlTree<T>();
